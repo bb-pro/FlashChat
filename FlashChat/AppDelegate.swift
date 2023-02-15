fb1f89f2e4d84a23e4668416757b965a5c5b504f
Bektemur Mamashaev <bektemurmamashayev07@gmail.com>
2023-02-15 21:38:51 +0500

    work with iqkeyboardmanager package

--- FlashChat/AppDelegate.swift
+++ FlashChat/AppDelegate.swift
@@ -9,6 +9,7 @@ import UIKit
 import FirebaseCore
 import FirebaseFirestore
 import FirebaseAuth
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -17,9 +18,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
-        
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
         let db = Firestore.firestore()
-        print(db)
         return true
     }
 
